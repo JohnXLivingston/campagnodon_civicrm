deba4546523dc38ea26e5f548ba64af74323433f
John Livingston <git@john-livingston.fr>
2022-05-06 17:11:44 +0200

    WIP.

--- api/v3/Campagnodon/Create.php
+++ api/v3/Campagnodon/Create.php
@@ -10,11 +10,80 @@ use CRM_CampagnodonCivicrm_ExtensionUtil as E;
  * @see https://docs.civicrm.org/dev/en/latest/framework/api-architecture/
  */
 function _civicrm_api3_campagnodon_Create_spec(&$spec) {
-  $spec['magicword']['api.required'] = 1;
+  $spec["first_name"] = [
+    "name" => "first_name",
+    "title" => ts("First name"),
+    "description" => ts("First name"),
+    "type" => CRM_Utils_Type::T_STRING,
+    "api.required" => 0,
+    "api.default" => "",
+  ];
+  $spec["last_name"] = [
+      "name" => "last_name",
+      "title" => ts("Last name"),
+      "description" => ts("Last name"),
+      "type" => CRM_Utils_Type::T_STRING,
+      "api.required" => 0,
+      "api.default" => "",
+  ];
+  $spec["email"] = [
+      "name" => "email",
+      "title" => ts("E-mail"),
+      "description" => ts("E-mail"),
+      "type" => CRM_Utils_Type::T_STRING,
+      "api.required" => 1,
+      "api.default" => "",
+  ];
+  $spec["postal_code"] = [
+      "name" => "postal_code",
+      "title" => ts("Postal code"),
+      "description" => ts("Postal code"),
+      "type" => CRM_Utils_Type::T_STRING,
+      "api.required" => 0,
+      "api.default" => "",
+  ];
+  $spec["country"] = [
+      "name" => "country",
+      "title" => ts("Country"),
+      "description" => "Country ISO code",
+      "type" => CRM_Utils_Type::T_STRING,
+      "api.required" => 0,
+      "api.default" => "",
+  ];
+  $spec["phone"] = [
+      "name" => "phone",
+      "title" => ts("Phone"),
+      "description" => "Phone",
+      "type" => CRM_Utils_Type::T_STRING,
+      "api.required" => 0,
+      "api.default" => "",
+  ];
+  $spec["transaction_idx"] = [
+      "name" => "transaction_idx",
+      "title" => ts("Donation external identifier"),
+      "description" => "Unique identifier",
+      "type" => CRM_Utils_Type::T_STRING,
+      "api.default" => "",
+  ];
+  $spec["campaign_id"] = [
+      "name" => "campaign_id",
+      "title" => ts("Campaign ID"),
+      "description" => "Unique campaign id",
+      "type" => CRM_Utils_Type::T_INT,
+      "api.default" => "",
+  ];
+  $spec["donation_amount"] = [
+      "title" => "Donation amount",
+      "type" => CRM_Utils_Type::T_INT,
+      "api.required" => 1,
+  ];
 }
 
 /**
  * Campagnodon.Create API
+ *
+ * Used to create a new donation/subscription/[...].
+ * Create the contact if not found, otherwise update it.
  *
  * @param array $params
  *
@@ -26,20 +95,68 @@ function _civicrm_api3_campagnodon_Create_spec(&$spec) {
  * @throws API_Exception
  */
 function civicrm_api3_campagnodon_Create($params) {
-  if (array_key_exists('magicword', $params) && $params['magicword'] == 'sesame') {
-    $returnValues = array(
-      // OK, return several data rows
-      12 => ['id' => 12, 'name' => 'Twelve'],
-      34 => ['id' => 34, 'name' => 'Thirty four'],
-      56 => ['id' => 56, 'name' => 'Fifty six'],
-    );
-    // ALTERNATIVE: $returnValues = []; // OK, success
-    // ALTERNATIVE: $returnValues = ["Some value"]; // OK, return a single value
+  $tx = new CRM_Core_Transaction();
+  try {
+    // checking if there is at least one type of donation, membership, ...
+    $donation_amount = intval($params['donation_amount'] ?? 0);
+    if ($donation_amount <= 0) {
+      throw new API_Exception('No donation');
+    }
+    // TODO: implement other type of subscriptions
 
-    // Spec: civicrm_api3_create_success($values = 1, $params = [], $entity = NULL, $action = NULL)
-    return civicrm_api3_create_success($returnValues, $params, 'Campagnodon', 'Create');
+    // We need to found or create the contact.
+    $contacts = civicrm_api3('Contact', 'get', array(
+      'email' => $params['email'],
+      'sequential' => true
+    ));
+
+    $contact = null;
+    // TODO: avoid update existing contact (in case paiment is not valid, to avoid database corruption attacks)
+    if ($contacts['count'] == 0) {
+      $contactsBis = civicrm_api3('Contact', 'create', array(
+        'email' => $params['email'],
+        'first_name' => $params['first_name'],
+        'last_name' => $params['last_name'],
+        // 'postal_code' => $params['postal_code'],
+        // 'country' => $params['country'],
+        'contact_type' => 'Individual',
+        'sequential' => true,
+        'options' => [
+          'reload' => true
+        ]
+      ));
+      if ($contactsBis['count'] === 1) {
+        $contact = $contactsBis['values'][0];
+      }
+    } else if ($contacts['count'] === 1) {
+      $contact = $contacts['values'][0];
+    } else if ($contacts['count'] > 1) {
+      throw new API_Exception('Multiple contacts found, deduplication code not ready. FIXME');
+    } else {
+      throw new API_Exception('Unknown error');
+    }
+
+    if (empty($contact)) {
+      throw new API_Exception('Failed to get or create the contact.');
+    }
+
+    // Now that we have a contact, we can make a donation.
+    if ($donation_amount > 0) {
+      // $contribution = civicrm_api3("contribution", "create", [
+      //   "financial_type_id" => 2,
+      //   "contact_id" => $params["contact_id"],
+      //   "total_amount" => $params["amount"] - 12,
+      // ]);
+    }
+
+    $result = [
+      'contact' => $contact
+    ];
+  } catch (Exception $e) {
+    $tx->rollback();
+    throw $e;
   }
-  else {
-    throw new API_Exception(/*error_message*/ 'Everyone knows that the magicword is "sesame"', /*error_code*/ 'magicword_incorrect');
-  }
+  $tx->commit();
+
+  return civicrm_api3_create_success($result, $params, 'Campagnodon', 'create');
 }
--- tests/phpunit/api/v3/Campagnodon/CreateTest.php
+++ tests/phpunit/api/v3/Campagnodon/CreateTest.php
@@ -41,13 +41,82 @@ class api_v3_Campagnodon_CreateTest extends \PHPUnit\Framework\TestCase implemen
   }
 
   /**
-   * Simple example test case.
-   *
-   * Note how the function name begins with the word "test".
+   * Provide contacts of unit tests.
    */
-  public function testApiExample() {
-    $result = civicrm_api3('Campagnodon', 'create', array('magicword' => 'sesame'));
-    $this->assertEquals('Twelve', $result['values'][12]['name']);
+  public function dataTestProviders() {
+    return [
+      'john' => [array(
+        'email' => 'john.doe@example.com',
+        'donation_amount' => 12
+      )],
+      'bill' => [array(
+        'email' => 'bill.smith@example.com',
+        'first_name' => 'Bill',
+        'last_name' => 'Smith',
+        'donation_amount' => 45
+      )]
+    ];
+  }
+
+  /**
+   * Test campagnodon create API. Must fail if no email provided.
+   */
+  public function testApiCreateWithoutEmail() {
+    $this->expectException(CiviCRM_API3_Exception::class);
+    $result = civicrm_api3('Campagnodon', 'create', array());
+  }
+
+  /**
+   * @dataProvider dataTestProviders
+   */
+  public function testApiCreate($params) {
+    $result = civicrm_api3('Campagnodon', 'create', $params);
+
+    $this->assertEquals(1, $result['count']);
+    $this->assertArrayHasKey('contact', $result['values']);
+    $contact = $result['values']['contact'];
+
+    $this->assertEquals($params['email'], $contact['email']);
+    $this->assertSame($params['first_name'] ?? '', $contact['first_name']);
+    $this->assertSame($params['last_name'] ?? '', $contact['last_name']);
+  }
+
+  /**
+   * Test campagnodon create API.
+   * Tests deduplication of contacts.
+   */
+  public function testApiCreateDedup() {
+    $result = civicrm_api3('Campagnodon', 'create', array(
+      'email' => 'john.doe@example.com',
+      'donation_amount' => 12
+    ));
+
+    $this->assertEquals(1, $result['count']);
+    $this->assertArrayHasKey('contact', $result['values']);
+
+    $contact_id = $result['values']['contact']['id'];
+    $this->assertTrue(intval($contact_id) > 0);
+
+    // Another donation, with a different contact.
+    $result = civicrm_api3('Campagnodon', 'create', array(
+      'email' => 'bill.smith@example.com',
+      'first_name' => 'Bill',
+      'last_name' => 'Smith',
+      'donation_amount' => 34
+    ));
+    $this->assertEquals(1, $result['count']);
+    $this->assertArrayHasKey('contact', $result['values']);
+    $this->assertTrue($contact_id != $result['values']['contact']['id']);
+
+    $result = civicrm_api3('Campagnodon', 'create', array(
+      'email' => 'john.doe@example.com',
+      'first_name' => 'John',
+      'last_name' => 'Doe',
+      'donation_amount' => 15
+    ));
+    $this->assertEquals(1, $result['count']);
+    $this->assertArrayHasKey('contact', $result['values']);
+    $this->assertSame($contact_id, $result['values']['contact']['id']);
   }
 
 }
