b20857e2008dc5976077714da590754248551565
John Livingston <git@john-livingston.fr>
2022-06-01 14:19:43 +0200

    Cleaning.

--- CRM/CampagnodonCivicrm/Page/Campagnodon.php
+++ CRM/CampagnodonCivicrm/Page/Campagnodon.php
@@ -7,9 +7,6 @@ class CRM_CampagnodonCivicrm_Page_Campagnodon extends CRM_Core_Page {
     // Example: Set the page-title dynamically; alternatively, declare a static title in xml/Menu/*.xml
     CRM_Utils_System::setTitle(E::ts('Campagnodon'));
 
-    // Example: Assign a variable for use in a template
-    $this->assign('currentTime', date('Y-m-d H:i:s'));
-
     parent::run();
   }
 
