8d76f18d216e28812c5cfe0d3ee2b5d6a03d8174
Pierre-Gabriel <pierre-gabriel@attac.org>
2024-07-01 14:11:40 +0200

    civix upgrade

--- campagnodon_civicrm.php
+++ campagnodon_civicrm.php
@@ -25,59 +25,12 @@ function campagnodon_civicrm_civicrm_install() {
 }
 
 /**
- * Implements hook_civicrm_postInstall().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_postInstall
- */
-function campagnodon_civicrm_civicrm_postInstall() {
-  _campagnodon_civicrm_civix_civicrm_postInstall();
-}
-
-/**
- * Implements hook_civicrm_uninstall().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_uninstall
- */
-function campagnodon_civicrm_civicrm_uninstall() {
-  _campagnodon_civicrm_civix_civicrm_uninstall();
-}
-
-/**
  * Implements hook_civicrm_enable().
  *
  * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_enable
  */
 function campagnodon_civicrm_civicrm_enable() {
   _campagnodon_civicrm_civix_civicrm_enable();
-}
-
-/**
- * Implements hook_civicrm_disable().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_disable
- */
-function campagnodon_civicrm_civicrm_disable() {
-  _campagnodon_civicrm_civix_civicrm_disable();
-}
-
-/**
- * Implements hook_civicrm_upgrade().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_upgrade
- */
-function campagnodon_civicrm_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
-  return _campagnodon_civicrm_civix_civicrm_upgrade($op, $queue);
-}
-
-/**
- * Implements hook_civicrm_entityTypes().
- *
- * Declare entity types provided by this module.
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_entityTypes
- */
-function campagnodon_civicrm_civicrm_entityTypes(&$entityTypes) {
-  _campagnodon_civicrm_civix_civicrm_entityTypes($entityTypes);
 }
 
 // --- Functions below this ship commented out. Uncomment as required. ---
